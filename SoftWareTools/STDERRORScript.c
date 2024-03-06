0e12d054f71b56636acd2bc2d520bcd66740ca6b
io23632 <io23632@bristol.ac.uk>
2024-03-06 17:00:10 +0000

    Added STd error script

---
+++ SoftWareTools/STDERRORScript.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    // Printing to standard output
+    printf("This is a standard output message.\n");
+
+    // Printing to standard error
+    fprintf(stderr, "This is a standard error message.\n");
+
+    return 0;
+}
+
